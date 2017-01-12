4630077dacc3c9023e27c8cc02d5c0445429a378
Shevah92 <shevah92@gmail.com>
2017-01-12 10:40:26 +0100

    all in every time

--- player.py
+++ player.py
@@ -4,11 +4,14 @@ class Player:
     name = "weneverwonanything"
     stack = 0
 
-
-
+    def get_info(self, game_state):
+        for player in game_state.players:
+            if player.name == self.name:
+                return player.stack
 
     def betRequest(self, game_state):
-        return game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet]
+        self.stack = self.get_info(game_state)
+        return self.stack - game_state.players[game_state.in_action][game_state.bet]
 
     def showdown(self, game_state):
         pass
