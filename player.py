02b5a6613920a6d2da8af3e873f6f9933c01b0e6
Shevah92 <shevah92@gmail.com>
2017-01-12 10:36:11 +0100

    add bet_request

--- player.py
+++ player.py
@@ -1,12 +1,18 @@
 
 class Player:
     VERSION = "Default Python rasing player"
+    name = "weneverwonanything"
+    stack = 0
+
 
 
 
     def betRequest(self, game_state):
-        return game_state.current_buy_in
+        return game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet]
 
     def showdown(self, game_state):
         pass
+
+
+
 
