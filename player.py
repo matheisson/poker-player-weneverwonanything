5abf2619ff86c5e472dced53550c966993c63214
Shevah92 <shevah92@gmail.com>
2017-01-12 16:47:34 +0100

    add flush checker

--- player.py
+++ player.py
@@ -29,10 +29,10 @@ class Player:
             if self.hand[0]["rank"] in self.good_cards:
                 if self.hand[0]["rank"] == self.hand[1]["rank"]:
                     self.counter += 1
-                    return 200
+                    return self.current_bid - self.bet
             if self.hand[1]["rank"] in self.good_cards:
                 self.counter += 1
-                return 10
+                return self.current_bid - self.bet
             return 0
         if self.hand[0]["rank"] in self.good_cards and self.hand[1]["rank"] in self.good_cards:
             if len(self.common_cards) == 0:
