7f35e574d37ac3169a5aefc5327fcdce272d7180
matetotesi <matet.totesi@gmail.com>
2017-01-12 12:12:38 +0100

    try to resolve error

--- player.py
+++ player.py
@@ -5,14 +5,10 @@ class Player:
     stack = 0
 
     def get_info(self, game_state):
-        current_buy_in = game_state["current_buy_in"]
-        print current_buy_in
+        pass
 
 
     def betRequest(self, gs):
-        current_buy_in = gs["current_buy_in"]
-        print current_buy_in
-        gs.current_buy_in - gs.players[gs.in_action].bet
         return 500
 
 
