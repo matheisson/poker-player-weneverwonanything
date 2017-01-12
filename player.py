3627872fb791657165f9951c48e154c87c783bac
Shevah92 <shevah92@gmail.com>
2017-01-12 10:23:30 +0100

    add bet_request

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet]
+        return game_state.current_buy_in - game_state.players[game_state.in_action][game_state.bet] + game_state.minimum_raise
 
     def showdown(self, game_state):
         pass
