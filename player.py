bcf5721756cb0b67c7cc37aa1ae3dd57ed6dcc41
Shevah92 <shevah92@gmail.com>
2017-01-12 17:14:34 +0100

    im tired

--- player.py
+++ player.py
@@ -29,10 +29,10 @@ class Player:
             if self.hand[0]["rank"] in self.good_cards:
                 if self.hand[0]["rank"] == self.hand[1]["rank"]:
                     self.counter += 1
-                    return self.current_bid - self.bet
+                    return 100
                 if self.hand[1]["rank"] in self.good_cards:
                     self.counter += 1
-                    return 100
+                    return 50
             if self.suit:
                 return 120
             return 0
