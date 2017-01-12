53322a42d741e32bbabb91b3fe59244fa4074dd1
matheisson <csanyil@gmail.com>
2017-01-12 17:25:58 +0100

    asdasdasd

--- player.py
+++ player.py
@@ -35,7 +35,7 @@ class Player:
                     return 50
             if self.suit:
                 return 50
-            return 20
+            return 35
         if self.hand[0]["rank"] in self.good_cards and self.hand[1]["rank"] in self.good_cards:
             if len(self.common_cards) == 0:
                 return 20
